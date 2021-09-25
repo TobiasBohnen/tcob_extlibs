9c3418b31389eafdd599786f49fc6c7614a1fa8d
TobiasBohnen <1294616+TobiasBohnen@users.noreply.github.com>
2021-09-25 20:29:59 +0200

    important changes

---
+++ TinySoundFont/TinySoundFont.c
@@ -0,0 +1,12 @@
+/**
+ * Copyright (c) 2021 Tobias Bohnen
+ * 
+ * This software is released under the MIT License.
+ * https://opensource.org/licenses/MIT
+ */
+
+#define TSF_IMPLEMENTATION
+#include "tsf.h"
+
+#define TML_IMPLEMENTATION
+#include "tml.h"
