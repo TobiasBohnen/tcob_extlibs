1c1e932ec1646315e1b2ac9df4a38d8c15cf141f
TobiasBohnen <1294616+TobiasBohnen@users.noreply.github.com>
2022-09-17 23:40:44 +0200

    bug fixes & performance improvements

--- litehtml/src/gumbo/visualc/include/strings.h
+++ litehtml/src/gumbo/visualc/include/strings.h
@@ -1,6 +1,4 @@
 /*Dummy file to satisfy source file dependencies on Windows platform*/
-#if defined(_MSC_VER)
-    #define strcasecmp _stricmp
-    #define strncasecmp _strnicmp
-    #define inline __inline
-#endif
+#define strcasecmp _stricmp
+#define strncasecmp _strnicmp
+#define inline __inline
