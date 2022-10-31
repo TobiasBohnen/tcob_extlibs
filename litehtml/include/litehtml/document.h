a274508e76eb6ec182b879868d12e0ad1e356bad
TobiasBohnen <1294616+TobiasBohnen@users.noreply.github.com>
2022-10-31 21:50:58 +0100

    bug fixes & performance improvements

--- litehtml/include/litehtml/document.h
+++ litehtml/include/litehtml/document.h
@@ -58,7 +58,8 @@ namespace litehtml
 		css_text::vector					m_css;
 		litehtml::css						m_styles;
 		litehtml::web_color					m_def_color;
-		litehtml::css						m_master_css;
+        litehtml::css						m_master_css;
+        litehtml::css						m_user_css;
 		litehtml::size						m_size;
 		position::vector					m_fixed_boxes;
 		media_query_list::vector			m_media_lists;
@@ -102,7 +103,7 @@ namespace litehtml
 
 		static litehtml::document::ptr	createFromString(const char* str, litehtml::document_container* objPainter, const char* master_styles = litehtml::master_css, const char* user_styles = "");
 	
-			//ADDED
+		//ADDED
 		void							refresh_render_tree();
 	private:
 		uint_ptr	add_font(const char* name, int size, const char* weight, const char* style, const char* decoration, font_metrics* fm);
