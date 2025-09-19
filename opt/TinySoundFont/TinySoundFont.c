a161b651b431b0547f570c91751a95801969a35a
Tobias Bohnen <1294616+TobiasBohnen@users.noreply.github.com>
2025-09-19 11:32:43 +0200

    remove opus/stb_vorbis

--- opt/TinySoundFont/TinySoundFont.c
+++ opt/TinySoundFont/TinySoundFont.c
@@ -5,11 +5,6 @@
  * https://opensource.org/licenses/MIT
  */
 
-#if defined (STB_VORBIS_NO_PUSHDATA_API)
-#define STB_VORBIS_HEADER_ONLY
-#include "stb_vorbis.c"
-#endif
-
 #define TSF_IMPLEMENTATION
 #include "tsf.h"
 
